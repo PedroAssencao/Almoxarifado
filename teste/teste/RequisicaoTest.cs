713ddf7b7365319a7ff4a6047240f0fc480c51ab
PedroAssencao <115711899+PedroAssencao@users.noreply.github.com>
2024-01-25 10:19:06 -0300

    a

--- teste/teste/RequisicaoTest.cs
+++ teste/teste/RequisicaoTest.cs
@@ -45,5 +45,26 @@ namespace teste
             Assert.Equal(resultado, veDepartamento);
         }
 
+  [Theory]
+   [InlineData("20")]
+   [InlineData("20")]
+   public void CT03R3RequisicaoQtdItem(string valorEsperado)
+   {
+       driver.Navigate().GoToUrl("https://splendorous-starlight-c2b50a.netlify.app/");
+       driver.Manage().Window.Size = new System.Drawing.Size(1936, 1048);
+       driver.FindElement(By.Id("CodigoProduto")).Click();
+       driver.FindElement(By.Id("CodigoProduto")).SendKeys("1");
+       driver.FindElement(By.Id("Quantidade")).Click();
+       driver.FindElement(By.Id("Quantidade")).SendKeys(valorEsperado);
+       driver.FindElement(By.CssSelector("#BtnInserirItens > span")).Click();
+       Thread.Sleep(3000);
+       IWebElement tabela = driver.FindElement(By.Id("tabelaItens"));
+       IWebElement celula = tabela.FindElement(By.XPath(".//tr[1]/td[3]"));
+       string valorEncontrado = celula.Text;
+       driver.Quit();
+
+       Assert.Equal(valorEsperado,valorEncontrado);
+   }
+
     }
 }
